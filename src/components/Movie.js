b50f3448bc3ec2ea97318723414935aa5285ef50
STUDENT_M16 <student_m16@PS-DAL-M16.local>
2020-08-18 10:43:13 -0500

    created movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie() {
+function Movie(id, title, summary, genres, year, poster) {
   return <h1></h1>;
 }
 
-Movie.PropTypes = {};
+Movie.PropTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  poster: PropTypes.string.isRequired,
+};
