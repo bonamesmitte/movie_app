3245093d8214ac36818154bf625448ba401fdd0a
STUDENT_M16 <student_m16@PS-DAL-M16.local>
2020-08-18 22:58:41 -0500

    changed App.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
 class Detail extends Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push('/');
+    }
+  }
   render() {
-    return <span>Hello Detail</span>;
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
   }
 }
 
