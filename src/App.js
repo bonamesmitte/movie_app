8f3709e2ada698ebee62014eca5982a939b42f39
STUDENT_M16 <student_m16@PS-DAL-M16.local>
2020-08-18 22:07:08 -0500

    used Link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
