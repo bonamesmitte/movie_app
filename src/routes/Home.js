a9b2bfaabd6ddf54460ebef20574b7a2e1504a87
STUDENT_M16 <student_m16@PS-DAL-M16.local>
2020-08-18 21:21:51 -0500

    change App.css's name

---
+++ src/routes/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from './components/Movie';
+import './App.css';
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.setState({ movies, isLoading: false });
+
+    // const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    // console.log(movies.data.data.movies);
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                genres={movie.genres}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+export default Home;
