76e074b4a0d621b179aaca38dd2486196df13838
STUDENT_M16 <student_m16@PS-DAL-M16.local>
2020-08-18 22:26:43 -0500

    added state

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,21 +5,34 @@ import { Link } from 'react-router-dom';
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="movie_genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres_genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie_summary">{summary.slice(0, 180)}...</p>
+    <Link
+      to={{
+        pathname: '/movie_detail',
+        state: {
+          year: year,
+          title: title,
+          summary: summary,
+          poster: poster,
+          genres: genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie_data">
+          <h3 className="movie_title">{title}</h3>
+          <h5 className="movie_year">{year}</h5>
+          <ul className="movie_genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres_genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie_summary">{summary.slice(0, 180)}...</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
