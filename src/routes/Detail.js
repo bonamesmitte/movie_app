174040fee6dfc92155c6398b9f6dae02b6192557
STUDENT_M16 <student_m16@PS-DAL-M16.local>
2020-08-18 22:46:49 -0500

    changed Detail

---
+++ src/routes/Detail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Detail extends Component {
+  render() {
+    return <span>Hello Detail</span>;
+  }
+}
+
+// function Detail({ location }) {
+//   console.log(location);
+//   return <span>Hello</span>;
+// }
+
+export default Detail;
